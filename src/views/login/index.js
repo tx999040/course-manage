607fc9c59c51c3272ef73aceb8ac7aaefa5afc6a
tangxiong <tx999040@163.com>
2022-08-01 15:24:05 +0800

    提交修改

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Input } from 'antd';
+import '../../styles/index.less';
 class Index extends React.Component{
 render(){
     return (
-        <div>
-            这是首页
+        <div className="app-container">
+         <div className="login">
+            <h5>密码登录</h5>
+        
+         <Input placeholder="请输入手机号" />
+         <Input placeholder="请输入密码" />
+         </div>
         </div>
     )
 }
@@ -11,3 +18,4 @@ render(){
  }
  
  export default Index;
+ 
